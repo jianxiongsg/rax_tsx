9ba3e99de4390f98f5030ebf0e198607d098dcad
jianxiong <jianxiong@lovigame.com>
2020-04-24 15:38:56 +0800

    up

--- src/components/Demo/index.tsx
+++ src/components/Demo/index.tsx
@@ -1,5 +1,5 @@
+/** @jsx createElement */
 import {Component,render,createElement} from "rax";
-import * as React from 'rax';
 import "./index.less"
 interface PropsInfo{
     data:string
@@ -10,7 +10,8 @@ interface StateInfo{
 
 export default class Demo extends Component<PropsInfo,StateInfo>{
     constructor(props:PropsInfo){
-        super(props)
+        super(props);
+        ["111","222"].map((v)=>{console.log(v)})
     }
     render(){
         return <div className="demo">1111111111111</div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
+/** @jsx createElement */
 import {createElement,render,Component} from "rax";
 import Demo from "./components/Demo";
 import * as DriverDOM from 'driver-dom';
-import * as React from 'rax';
-(window as any).React = React;
 export function start(ele:HTMLElement){    
     render(
         <Demo data={"111"} />
